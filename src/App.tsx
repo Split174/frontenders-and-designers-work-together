407d47552222262bb43a27996b1c1d11c0df6a24
sadcitizen <sadcitizen@yandex.ru>
2025-12-25 13:23:00 +0500

    setup fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import styles from './App.module.css';
+
 export function App() {
     return (
-        <>
+        <section className={styles.container}>
             <p>Привет, воспользуйся storybook для разработки.</p>
             <p>
-                Её можно запустить командой <code>npm run storybook</code>
+                Её можно запустить командой <code className={styles.code}>npm run storybook</code>
             </p>
-        </>
+        </section>
     );
 }
